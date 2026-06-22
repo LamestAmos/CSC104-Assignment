ea36e857a0facafd95724fa24034525d42b84f64
LamestAmos <mubaraksadudeen@gmail.com>
2026-06-22 11:36:05 +0100

    feat(q3): Added some Initial code
    The quiz can only for now traverse through the list. correct answers and grading/scoring W.I.P

---
+++ question-3/README.md
@@ -0,0 +1,7 @@
+## Question 3
+
+Create an **interactive multiple-choice quiz** with at least 4 questions.
+Show one question at a time. When the user selects an answer, the
+background should turn **green** for correct or **red** for wrong before
+moving to the next question. Display the final score and a grade
+remark at the end.
---
+++ question-3/src/index.js
@@ -0,0 +1,78 @@
+const questions = [
+  {
+    title: "What is 1 + 1?",
+    answer: "C",
+    options: ["A. 11", "B. 10", "C. 2", "D. Idk"],
+  },
+  {
+    title: "What is 1 + 1?",
+    answer: "C",
+    info: "Do the thing with the thing.",
+    options: ["A. 11", "B. 10", "C. 2", "D. Idk"],
+  },
+  {
+    title: "What is 1 + 1?",
+    answer: "C",
+    options: ["A. 11", "B. 10", "C. 2", "D. Idk"],
+  },
+  {
+    title: "What is 1 + 1?",
+    answer: "C",
+    options: ["A. 11", "B. 10", "C. 2", "D. Idk"],
+  },
+];
+
+let questionIndex = 0;
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const questionCard = document.querySelector(".question-card");
+
+update();
+
+nextBtn.addEventListener("click", () => {
+  questionIndex++;
+  update();
+});
+
+prevBtn.addEventListener("click", () => {
+  questionIndex--;
+  update();
+});
+
+questionCard.addEventListener("click", (e) => {
+  if (!e.target.matches("button")) return;
+  const answer = e.target.innerText.charAt(0);
+  console.log(checkAnswer(answer));
+});
+
+function updateControls() {
+  nextBtn.disabled = questionIndex >= questions.length - 1;
+  prevBtn.disabled = questionIndex <= 0;
+}
+
+function updateQuestion() {
+  const question = questions[questionIndex];
+  if (!question) return;
+  const { title, answer, options, info = "" } = question;
+  questionCard.innerHTML = `
+        <h2>${questionIndex + 1}.</h2>
+        <h3>${title}</h3>
+        <p>
+          ${info}
+        </p>
+        <ul>
+        ${options.map((option) => `<li><button class="option">${option}</button></li>`).join("")}
+        </ul>
+`;
+}
+
+function checkAnswer(answer) {
+  const question = questions[questionIndex];
+  if (!question) return false;
+  return answer === question.answer;
+}
+
+function update({ controls = true, questions = true } = {}) {
+  questions && updateQuestion();
+  controls && updateControls();
+}
